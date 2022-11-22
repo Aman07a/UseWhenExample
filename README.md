2d8a0fd4a264bdc0b7b96ce8a8af872243d7c0e2
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-22 16:38:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UseWhenExample
+ Asp.Net Core 7 (.NET 7) | True Ultimate Guide
---
+++ UseWhenExample/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseWhen(
+    context => context.Request.Query.ContainsKey("username"),
+    app =>
+    {
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("Hello from Middleware branch\n");
+            await next();
+        });
+    });
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello from middleware at main chain\n");
+});
+
+app.Run();
